f8f52d710d030a7cabf403a5efa6b676295cc02f
aiXing <862099730@qq.com>
2016-04-05 09:45:25 +0800

    Initial Commit

---
+++ AXAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXAlertView
+//
+//  Created by ai on 16/4/5.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXAlertView
+//
+//  Created by ai on 16/4/5.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
