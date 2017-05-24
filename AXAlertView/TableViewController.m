83c9d65dc64a0bdf36f08e01e1bcb4816373708c
devedbox <862099730@qq.com>
2017-05-24 21:36:26 +0800

    Updated sample codes.

--- AXAlertView/TableViewController.m
+++ AXAlertView/TableViewController.m
@@ -23,8 +23,9 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
-    self.tableView.backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Image"]];
+    UIView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Image"]];
+    backgroundView.contentMode = UIViewContentModeScaleAspectFill;
+    self.tableView.backgroundView = backgroundView;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -72,18 +73,56 @@
     confirmConfig.backgroundColor = [UIColor blackColor];
     confirmConfig.preferedHeight = 44;
     confirmConfig.cornerRadius = .0;
-    confirmConfig.tintColor = [UIColor blackColor];
+    // confirmConfig.tintColor = [UIColor blackColor];
+    confirmConfig.tintColor = [UIColor whiteColor];
+    confirmConfig.translucentStyle = AXAlertViewTranslucentDark;
     [alertView setActionConfiguration:confirmConfig forItemAtIndex:1];
-    [alertView showInView:self.navigationController.view animated:YES];
+    [alertView showInView:self.view animated:YES];
+}
+
+- (IBAction)showNormalWithoutCustomView:(id)sender {
+    AXAlertView *alertView = [[AXAlertView alloc] initWithFrame:self.view.bounds];
+    alertView.customViewInset = UIEdgeInsetsMake(5, 20, 10, 20);
+    alertView.padding = 5;
+    alertView.cornerRadius = 10.0;
+    alertView.actionItemMargin = 0;
+    alertView.actionItemPadding = 0;
+    alertView.titleLabel.numberOfLines = 0;
+    alertView.hidesOnTouch = YES;
+    alertView.title = @"Some title content...";
+    alertView.titleLabel.font = [UIFont systemFontOfSize:14];
+    alertView.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size:18];
+    alertView.titleLabel.textColor = [UIColor blackColor];
+    
+    [alertView setActions:[AXAlertViewAction actionWithTitle:@"取消" image:nil handler:NULL],[AXAlertViewAction actionWithTitle:@"确认" image:nil handler:NULL],nil];
+    
+    AXAlertViewActionConfiguration *cancelConfig = [AXAlertViewActionConfiguration new];
+    cancelConfig.backgroundColor = [UIColor colorWithRed:0.949 green:0.949 blue:0.949 alpha:1.00];
+    cancelConfig.preferedHeight = 44;
+    cancelConfig.cornerRadius = .0;
+    cancelConfig.tintColor = [UIColor blackColor];
+    [alertView setActionConfiguration:cancelConfig forItemAtIndex:0];
+    AXAlertViewActionConfiguration *confirmConfig = [AXAlertViewActionConfiguration new];
+    confirmConfig.backgroundColor = [UIColor blackColor];
+    confirmConfig.preferedHeight = 44;
+    confirmConfig.cornerRadius = .0;
+    confirmConfig.tintColor = [UIColor blackColor];
+    // confirmConfig.tintColor = [UIColor whiteColor];
+    // confirmConfig.translucentStyle = AXAlertViewTranslucentDark;
+    [alertView setActionConfiguration:confirmConfig forItemAtIndex:1];
+    [alertView showInView:self.view animated:YES];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     switch (indexPath.row) {
         case 0: {
-            [self showNormal:nil];
+            [self showNormal:cell];
         } break;
-            
+        case 1: {
+            [self showNormalWithoutCustomView:cell];
+        } break;
         default:
             break;
     }
