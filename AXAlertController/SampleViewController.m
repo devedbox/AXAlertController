24adff7ad41e3b9a58637757e0d2b5e4150d42ab
devedbox <devedbox@gmail.com>
2017-06-20 15:59:45 +0800

    Added show with image type.

--- AXAlertController/SampleViewController.m
+++ AXAlertController/SampleViewController.m
@@ -31,11 +31,22 @@
 #pragma mark - UITableViewDelegate.
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    switch (indexPath.row) {
+        case 0: {// Show normal.
+            [self showNormal:cell];
+        } break;
+        case 1: {// With image.
+            [self showWithImage:cell];
+        } break;
+        default:
+            break;
+    }
 }
 
 #pragma mark - Actions.
 - (IBAction)showNormal:(id)sender {
-    AXAlertController *alert = [AXAlertController alertControllerWithTitle:@"Some title..." message:@"Some message..." preferredStyle:UIAlertControllerStyleAlert];
+    AXAlertController *alert = [AXAlertController alertControllerWithTitle:@"Some title..." message:@"Some message..." preferredStyle:AXAlertControllerStyleAlert];
     [alert addAction:[AXAlertAction actionWithTitle:@"Cancel" style:AXAlertActionStyleDefault handler:NULL] configurationHandler:^(AXAlertActionConfiguration * _Nonnull config) {
         config.preferedHeight = 44.0;
         config.backgroundColor = [UIColor whiteColor];
@@ -49,5 +60,41 @@
     }];
     
     [self presentViewController:alert animated:YES completion:NULL];
+}
+
+- (IBAction)showWithImage:(id)sender {
+    AXAlertController *alert = [AXAlertController alertControllerWithTitle:@"Some title..." message:@"Some message..." preferredStyle:AXAlertControllerStyleAlert];
+    [alert configureImageViewWithHandler:^(UIImageView * _Nonnull imageView) {
+        imageView.contentMode = UIViewContentModeCenter;
+        imageView.image = [self _resizedTouchImage];
+    }];
+    [alert addAction:[AXAlertAction actionWithTitle:@"Cancel" style:AXAlertActionStyleDefault handler:NULL] configurationHandler:^(AXAlertActionConfiguration * _Nonnull config) {
+        config.preferedHeight = 44.0;
+        config.backgroundColor = [UIColor whiteColor];
+        config.font = [UIFont boldSystemFontOfSize:17];
+        config.cornerRadius = .0;
+        config.tintColor = [UIColor colorWithRed:0 green:0.48 blue:1 alpha:1];
+    }];
+    [alert addAction:[AXAlertAction actionWithTitle:@"OK" style:AXAlertActionStyleDefault handler:NULL] configurationHandler:^(AXAlertActionConfiguration * _Nonnull config) {
+        config.font = [UIFont systemFontOfSize:17];
+        config.tintColor = [UIColor colorWithRed:0 green:0.48 blue:1 alpha:1];
+    }];
+    
+    [self presentViewController:alert animated:YES completion:NULL];
+}
+
+#pragma mark - Private.
+- (UIImage *)_resizedTouchImage {
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(44.0, 44.0), NO, [UIScreen mainScreen].scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, 0, 44.0);
+    CGContextScaleCTM(context, 1.0, -1);
+    CGContextSetFillColorWithColor(context, [UIColor orangeColor].CGColor);
+    CGContextDrawImage(context, CGRectMake(0, 0, 44.0, 44.0), [UIImage imageNamed:@"touch"].CGImage);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    CGContextRestoreGState(context);
+    UIGraphicsEndImageContext();
+    return image;
 }
 @end
